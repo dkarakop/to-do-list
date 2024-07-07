949c5ec2ca0f4b6b135f0bc8bc37bf565dddf775
Dimitra Karakopoulou <126284048+dkarakop@users.noreply.github.com>
2024-07-07 18:57:33 +0300

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import ToDoApp from "./components/ToDoApp";
 
-function App() {
+export default function App() {
 	return (
 		<div className="App">
 			<ToDoApp />
 		</div>
 	);
 }
-
-export default App;
