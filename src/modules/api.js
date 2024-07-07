2187bb42e344332651dd713822cbc4ed13d1ae17
Dimitra Karakopoulou <126284048+dkarakop@users.noreply.github.com>
2024-07-07 18:06:20 +0300

    Add comments in api.js

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,9 +1,20 @@
+// Retrieve the API URL from the environment variables
 const apiUrl = process.env.REACT_APP_API_URL;
 
+/**
+ * Getting the "toDos" array from our database.
+ * @param none
+ * @returns a Promise
+ */
 export const getToDoItems = () => {
 	return fetch(`${apiUrl}/toDos`).then((response) => response.json());
 };
 
+/**
+ * Adding a newly created toDo object to the 'toDos' array in our database. The new object's properties are: id: a new unique id, text: the provided text, completed: false.
+ * @param text
+ * @returns a Promise
+ */
 export function createToDoItem(text) {
 	const newId = String(Date.now());
 
@@ -16,12 +27,22 @@ export function createToDoItem(text) {
 	}).then((response) => response.json());
 }
 
+/**
+ * Removing a specific toDo object from the 'toDos' array in our database based on its id.
+ * @param id
+ * @returns a Promise
+ */
 export function deleteToDoItem(id) {
 	return fetch(`${apiUrl}/toDos/${id}`, {
 		method: "DELETE",
 	});
 }
 
+/**
+ * Updating the existing toDo object based on its id from the 'toDos' array in our database.
+ * @param toDo
+ * @returns a Promise
+ */
 export function editToDoItem(toDo) {
 	return fetch(`${apiUrl}/toDos/${toDo.id}`, {
 		method: "PUT",
